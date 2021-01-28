29b0f34ced1f2f648e817dff743941534c509f09
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-28 19:15:15 +0200

    Get random quote

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import discord
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 import os
+import json
 
 client = discord.Client()
+
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+    return quote
 
 
 @client.event
@@ -16,8 +24,9 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
 
 client.run(os.getenv('TOKEN'))
