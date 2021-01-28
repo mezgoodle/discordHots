8f9a71ae759ef46aea72f98ab086a124714ab93f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-28 19:31:07 +0200

    Add more functionality

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ import requests
 load_dotenv()
 import os
 import json
+import random
 
 client = discord.Client()
+
+sad_words = ['sad', 'depressed', 'unhappy', 'angry', 'miserable', 'depressing']
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
+
 
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
     json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+    quote = json_data[0]['q'] + ' - ' + json_data[0]['a']
     return quote
 
 
@@ -24,9 +34,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$inspire'):
+    msg = message.content
+    if msg.startswith('$inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
 
 
 client.run(os.getenv('TOKEN'))
