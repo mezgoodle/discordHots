560672716a228b161a1d3b80b12c73f5ff21a87c
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-01-29 07:40:27 +0000

    Responding variable

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ starter_encouragements = [
   "You are a great person / bot!"
 ]
 
+if 'responding' not in db.keys():
+    db['responding'] = True
+
 
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
@@ -56,12 +59,13 @@ async def on_message(message):
         quote = get_quote()
         await message.channel.send(quote)
 
-    options = starter_encouragements
-    if 'encouragments' in db.keys():
-        options += db['encouragments']
+    if db['responding']:
+        options = starter_encouragements
+        if 'encouragments' in db.keys():
+            options += db['encouragments']
 
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(options))
+        if any(word in msg for word in sad_words):
+            await message.channel.send(random.choice(options))
 
     if msg.startswith('$new'):
         encouraging_message = msg.split('$new ', 1)[1]
@@ -75,6 +79,23 @@ async def on_message(message):
             delete_encouragement(index)
             encouragments = db['encouragments']
         await message.channel.send(encouragments)
+
+    if msg.startswith('$list'):
+        encouragments = []
+        if 'encouragments' in db.keys():
+            encouragments = db['encouragments']
+        await message.channel.send(encouragments)
+
+    if msg.startswith('$responding'):
+        value = msg.split('$responding ', 1)[1]
+
+        if value.lower() == 'true':
+            db['responding'] = True
+            await message.channel.send('Responding is on')
+        else:
+            db['responding'] = False
+            await message.channel.send('Responding is off')
+
 
 
 client.run(os.getenv('TOKEN'))
